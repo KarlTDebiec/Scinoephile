792062ac5d1f2d1353e63b372ffcda022c939bdd
Karl Debiec <karl.t.debiec@gmail.com>
2017-12-30 16:44:55 -0600

    organizing

---
+++ add_pinyin.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#   add_pinyin.py
+#
+#   Copyright (C) 2017-2018 Karl T Debiec
+#   All rights reserved.
+#
+#   This software may be modified and distributed under the terms of the
+#   BSD license. See the LICENSE file for details.
+################################### MODULES ###################################
+################################### CLASSES ###################################
+class SubtitleManager(object):
+    def main():
+        pass
+#################################### MAIN #####################################
+if __name__ == "__main__":
+    SubtitleManager().main()
