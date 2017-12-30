0aa7c62124a7b5c0f4a2333706559334c6ebfdc1
Karl Debiec <karl.t.debiec@gmail.com>
2017-12-30 16:54:51 -0600

    organizing

---
+++ setup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="ZYSYZM", version="0.1", packages=["zysyzm"],
+  license="BSD 3-clause", long_description=open("README.rst").read())
