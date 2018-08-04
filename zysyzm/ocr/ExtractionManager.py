1149dded4e08c4b0982681f3e76d8bd497133306
Karl Debiec <karl.t.debiec@gmail.com>
2018-08-04 18:09:20 -0500

    ExtractionManager cleaned up

--- zysyzm/ocr/ExtractionManager.py
+++ zysyzm/ocr/ExtractionManager.py
@@ -8,18 +8,13 @@
 #   This software may be modified and distributed under the terms of the
 #   BSD license. See the LICENSE file for details.
 ################################### MODULES ###################################
-import numpy as np
-from glob import iglob
-from os import mkdir
-from os.path import basename, expandvars, isdir
-from IPython import embed
-from PIL import Image, ImageChops, ImageDraw
 from zysyzm import CLToolBase
+from zysyzm.ocr import resize_image, trim_image
 
 
 ################################### CLASSES ###################################
 class ExtractionManager(CLToolBase):
-    """ Extracts characters from image-based subtitles """
+    """Extracts characters from image-based subtitles"""
 
     # region Instance Variables
     help_message = ("Extracts characters from image-based subtitles")
@@ -27,48 +22,52 @@ class ExtractionManager(CLToolBase):
     # endregion
 
     # region Builtins
+    def __init__(self, input_directory=None, output_directory=None, **kwargs):
+        """
+        Initializes tool
+
+        Args:
+            input_directory (str): Path to directory containing input subtitle
+              images
+            output_directory (str): Path to directory for output character
+              images
+            **kwargs (dict): Additional keyword arguments
+        """
+        super().__init__(**kwargs)
+
+        self.input_directory = input_directory
+        self.output_directory = output_directory
+
     def __call__(self):
-        def trim(image):
-            background = Image.new(image.mode, image.size,
-                                   image.getpixel((0, 0)))
-            diff = ImageChops.difference(image, background)
-            diff = ImageChops.add(diff, diff, 2.0, -100)
-            bbox = diff.getbbox()
-            if bbox:
-                return image.crop(bbox)
+        """Core logic"""
+        import numpy as np
+        from os import mkdir
+        from os.path import basename, isdir
+        from PIL import Image, ImageDraw
 
-        def resize_image(image, new_size):
-            x = int(np.floor((new_size[0] - image.size[0]) / 2))
-            y = int(np.floor((new_size[1] - image.size[1]) / 2))
-            new_image = Image.new(image.mode, new_size, image.getpixel((0, 0)))
-            new_image.paste(image, (x, y, x + image.size[0], y + image.size[1]))
-            return new_image
-
-        indir = expandvars("${DROPBOX}/code/subtitles/mcdull_prince_de_la_bun/cmn-Hans")
-        # indir = "${DROPBOX}/code/subtitles/mcdull_kung_fu_ding_ding_dong/cmn-Hans"
-        # indir = "${DROPBOX}/code/subtitles/magnificent_mcdull/cmn-Hans"
-        base_outdir = expandvars("${HOME}/desktop/docs/subtitles/")
         threshold = 0.04
 
         if self.verbosity >= 1:
-            print(f"Processing subtitle image files in '{indir}'")
-            print(f"Saving individual character files to '{base_outdir}'")
+            print("Processing subtitle image files in "
+                  f"'{self.input_directory}'")
+            print("Saving character image files  to "
+                  f"'{self.output_directory}'")
 
-        for infile in sorted(iglob("{0}/*.png".format(indir))):
+        # Loop over subtitles
+        for infile in self.input_images:
             if self.verbosity >= 1:
                 print(f"Processing '{basename(infile)}'")
-            outdir = "{0}/{1}".format(base_outdir,
-                                      basename(infile).rstrip(".png"))
-            if not isdir(outdir):
-                mkdir(outdir)
 
             # Open image
-            image = Image.open(infile).convert("LA")
-            image = trim(image)
-            draw = ImageDraw.Draw(image)
+            subtitle_img = trim_image(Image.open(infile).convert("LA"))
+            subtitle_output_directory = \
+                f"{self.output_directory}/{basename(infile).rstrip('.png')}"
+            if not isdir(subtitle_output_directory):
+                mkdir(subtitle_output_directory)
+            subtitle_outfile = f"{subtitle_output_directory}/full.png"
 
             # Identify blank columns containing only white pixels
-            raw = np.asarray(image)[:, :, 0]
+            raw = np.asarray(subtitle_img)[:, :, 0]
             nonwhite_pixels_in_column = (raw < raw.max()).sum(axis=0)
             blank_columns = np.where(nonwhite_pixels_in_column == 0)[0]
             if len(blank_columns) == 0:
@@ -81,10 +80,11 @@ class ExtractionManager(CLToolBase):
             char_boundaries = np.array([0])
             char_width = 75
 
+            # Loop over characters in subtitle
             while True:
                 # Estimate location of next boundary between characters
                 next_boundary = char_boundaries[-1] + char_width
-                if next_boundary >= image.size[0] - (char_width / 2):
+                if next_boundary >= subtitle_img.size[0] - (char_width / 2):
                     break
 
                 # Identify blank columns closest to estimated boundary
@@ -133,22 +133,116 @@ class ExtractionManager(CLToolBase):
                 else:
                     char_width = 75
                 next_boundary = char_boundaries[-1] + char_width
-            char_boundaries = np.append(char_boundaries, image.size[0])
+            char_boundaries = np.append(char_boundaries, subtitle_img.size[0])
 
-            outfile = f"{outdir}/full.png"
+            # Write annotated subtitle image
+            draw = ImageDraw.Draw(subtitle_img)
             for boundary in char_boundaries[1:]:
-                draw.line((boundary, 0, boundary, image.size[1]),
+                draw.line((boundary, 0, boundary, subtitle_img.size[1]),
                           fill=0, width=2)
-            image.save(outfile)
+            subtitle_img.save(subtitle_outfile)
 
+            # Write individual character images
+            j = 0
             for i in range(len(char_boundaries) - 1):
-                char = image.crop((char_boundaries[i], 0,
-                                   char_boundaries[i + 1], image.size[1]))
-                char = trim(trim(char))
+                char = subtitle_img.crop((char_boundaries[i],
+                                          0,
+                                          char_boundaries[i + 1],
+                                          subtitle_img.size[1]))
+                char = trim_image(trim_image(char))  # Unclear why 2X needed
                 if char is not None:
-                    char = resize_image(char, (80,80))
-                    char.save(f"{outdir}/{i:02d}.png")
+                    char = resize_image(char, (80, 80))
+                    char.save(f"{subtitle_output_directory}/{j:02d}.png")
+                    j += 1
 
+    # endregion
+
+    # region Properties
+    @property
+    def input_directory(self):
+        """str: Path to directory containing input subtitle images"""
+        if not hasattr(self, "_input_directory"):
+            self._input_directory = None
+        return self._input_directory
+
+    @input_directory.setter
+    def input_directory(self, value):
+        from os.path import expandvars, isdir
+
+        if not isinstance(value, str) and value is not None:
+            raise ValueError()
+        else:
+            value = expandvars(value)
+            if not isdir(value):
+                raise ValueError()
+        self._input_directory = value
+
+    @property
+    def input_images(self):
+        from glob import iglob
+
+        return sorted(iglob("{0}/*.png".format(self.input_directory)))
+
+    @property
+    def output_directory(self):
+        """str: Path to directory for output character images"""
+        if not hasattr(self, "_output_directory"):
+            self._output_directory = None
+        return self._output_directory
+
+    @output_directory.setter
+    def output_directory(self, value):
+        from os import makedirs
+        from os.path import expandvars, isdir
+
+        if not isinstance(value, str) and value is not None:
+            raise ValueError()
+        else:
+            value = expandvars(value)
+            if not isdir(value):
+                try:
+                    makedirs(value)
+                except Exception as e:
+                    raise ValueError()
+        self._output_directory = value
+
+    # endregion
+
+    # region Class Methods
+    @classmethod
+    def construct_argparser(cls, parser=None):
+        """
+        Constructs argument parser
+
+        Returns:
+            parser (argparse.ArgumentParser): Argument parser
+        """
+        import argparse
+
+        # Prepare parser
+        if isinstance(parser, argparse.ArgumentParser):
+            parser = parser
+        elif isinstance(parser, argparse._SubParsersAction):
+            parser = parser.add_parser(name="extraction",
+                                       description=cls.help_message,
+                                       help=cls.help_message)
+        elif parser is None:
+            parser = argparse.ArgumentParser(description=cls.help_message)
+        super().construct_argparser(parser)
+
+        # Input
+        parser_inp = parser.add_argument_group("input arguments")
+        parser_inp.add_argument("-i", "--input_directory",
+                                type=str, required=True,
+                                help="path to input directory")
+
+        # Output
+        parser_out = parser.add_argument_group("output arguments")
+        parser_out.add_argument("-o", "--output_directory",
+                                type=str, required=True,
+                                help="path to output directory")
+
+        return parser
     # endregion
 
 
