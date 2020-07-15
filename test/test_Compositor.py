58af15e7c1073691552674614bb34363d04d7a48
Karl Debiec <karl.t.debiec@gmail.com>
2020-07-14 21:51:25 -0400

    New test set for test_Compositor.py

--- test/test_Compositor.py
+++ test/test_Compositor.py
@@ -23,12 +23,18 @@ observed_output_directory = expandvars("$SUBTITLES_ROOT/observed_output/")
 ################################## VARIABLES ##################################
 Movie = namedtuple("Movie", ["input", "output"])
 
-king_of_beggars = Movie(f"{input_directory}/king_of_beggars",
-                        f"{observed_output_directory}/king_of_beggars")
-saving_mr_wu = Movie(f"{input_directory}/saving_mr_wu",
-                     f"{observed_output_directory}/saving_mr_wu")
-trivisa = Movie(f"{input_directory}/trivisa",
-                f"{observed_output_directory}/trivisa")
+king_of_beggars = Movie(
+    f"{input_directory}/king_of_beggars",
+    f"{observed_output_directory}/king_of_beggars")
+mcdull_prince_de_la_bun = Movie(
+    f"{input_directory}/mcdull_prince_de_la_bun",
+    f"{observed_output_directory}/mcdull_prince_de_la_bun")
+saving_mr_wu = Movie(
+    f"{input_directory}/saving_mr_wu",
+    f"{observed_output_directory}/saving_mr_wu")
+trivisa = Movie(
+    f"{input_directory}/trivisa",
+    f"{observed_output_directory}/trivisa")
 
 
 #################################### TESTS ####################################
@@ -45,40 +51,51 @@ def test_argparser_missing(**kwargs: Any) -> None:
 
 @pytest.mark.parametrize(
     "bilingual_infile",
-    [f"{m.input}/cmn-hans_en.srt" for m in [saving_mr_wu, trivisa]])
+    [f"{m.input}/cmn-hans_en.srt"
+     for m in [mcdull_prince_de_la_bun, saving_mr_wu, trivisa]])
 def test_read_bilingual(bilingual_infile: str, **kwargs: Any) -> None:
     Compositor(bilingual_infile=bilingual_infile, **kwargs)()
 
 
 @pytest.mark.parametrize(
     "english_infile",
-    [f"{m.input}/en.srt" for m in [king_of_beggars, saving_mr_wu, trivisa]])
+    [f"{m.input}/en.srt"
+     for m in [king_of_beggars, mcdull_prince_de_la_bun, saving_mr_wu,
+               trivisa]])
 def test_read_english(english_infile: str, **kwargs: Any) -> None:
     Compositor(english_infile=english_infile, **kwargs)()
 
 
 @pytest.mark.parametrize(
     "hanzi_infile",
-    [f"{m.input}/cmn-hans.srt" for m in [saving_mr_wu, trivisa]] +
-    [f"{m.input}/yue-hans.srt" for m in [king_of_beggars]])
+    [f"{m.input}/cmn-hans.srt"
+     for m in [mcdull_prince_de_la_bun, saving_mr_wu, trivisa]] +
+    [f"{m.input}/yue-hans.srt"
+     for m in [king_of_beggars]])
 def test_read_hanzi(hanzi_infile: str, **kwargs: Any) -> None:
     Compositor(hanzi_infile=hanzi_infile, **kwargs)()
 
 
 @pytest.mark.parametrize(
     "pinyin_infile",
-    [f"{m.input}/cmn-pinyin.srt" for m in [saving_mr_wu]] +
-    [f"{m.input}/yue-pinyin.srt" for m in [king_of_beggars]])
+    [f"{m.input}/cmn-pinyin.srt"
+     for m in [saving_mr_wu]] +
+    [f"{m.input}/yue-pinyin.srt"
+     for m in [king_of_beggars]])
 def test_read_pinyin(pinyin_infile: str, **kwargs: Any) -> None:
     Compositor(pinyin_infile=pinyin_infile, **kwargs)()
 
 
 @pytest.mark.parametrize(
     ("bilingual_outfile", "english_infile", "hanzi_infile"),
-    [(f"{m.output}/cmn-hans_en.srt", f"{m.input}/en.srt",
-      f"{m.input}/cmn-Hans.srt") for m in [saving_mr_wu, trivisa]] +
-    [(f"{m.output}/yue-hans_en.srt", f"{m.input}/en.srt",
-      f"{m.input}/yue-Hans.srt") for m in [king_of_beggars]])
+    [(f"{m.output}/cmn-hans_en.srt",
+      f"{m.input}/en.srt",
+      f"{m.input}/cmn-Hans.srt")
+     for m in [mcdull_prince_de_la_bun, saving_mr_wu, trivisa]] +
+    [(f"{m.output}/yue-hans_en.srt",
+      f"{m.input}/en.srt",
+      f"{m.input}/yue-Hans.srt")
+     for m in [king_of_beggars]])
 def test_merge_hanzi_english(bilingual_outfile: str, english_infile: str,
                              hanzi_infile: str, **kwargs: Any) -> None:
     Compositor(bilingual_outfile=bilingual_outfile,
@@ -89,9 +106,13 @@ def test_merge_hanzi_english(bilingual_outfile: str, english_infile: str,
 
 @pytest.mark.parametrize(
     ("hanzi_infile", "pinyin_language", "pinyin_outfile"),
-    [(f"{m.input}/cmn-hans.srt", "mandarin", f"{m.output}/cmn-pinyin.srt")
+    [(f"{m.input}/cmn-hans.srt",
+      "mandarin",
+      f"{m.output}/cmn-pinyin.srt")
      for m in [saving_mr_wu]] +
-    [(f"{m.input}/yue-hans.srt", "cantonese", f"{m.output}/yue-pinyin.srt")
+    [(f"{m.input}/yue-hans.srt",
+      "cantonese",
+      f"{m.output}/yue-pinyin.srt")
      for m in [king_of_beggars]])
 def test_read_hanzi_write_pinyin(hanzi_infile: str, pinyin_language: str,
                                  pinyin_outfile: str, **kwargs: Any) -> None:
