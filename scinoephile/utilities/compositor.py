8bb579f4a4a8c7bfa0e1aaf6c19bb896233bdf97
Karl Debiec <karl.t.debiec@gmail.com>
2024-11-11 09:32:06 +0800

    wip

--- scinoephile/utilities/compositor.py
+++
@@ -1,8 +0,0 @@
-#  Copyright 2017-2024 Karl T Debiec. All rights reserved. This software may be modified
-#  and distributed under the terms of the BSD license. See the LICENSE file for details.
-from __future__ import annotations
-
-
-class Compositor:
-
-    pass
