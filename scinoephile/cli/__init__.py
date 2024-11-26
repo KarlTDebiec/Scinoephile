5944dd3480dc03d892b36ceb61e09413a22f4ebc
Karl Debiec <karl.t.debiec@gmail.com>
2024-11-26 08:50:33 -0500

    wip

--- scinoephile/cli/__init__.py
+++ scinoephile/cli/__init__.py
@@ -1,6 +1,6 @@
 #  Copyright 2017-2024 Karl T Debiec. All rights reserved. This software may be modified
 #  and distributed under the terms of the BSD license. See the LICENSE file for details.
-"""Command-line interfaces for scinoephile."""
+"""Command-line interface for scinoephile."""
 from __future__ import annotations
 
 from scinoephile.cli.scinoephile_cli import ScinoephileCli
