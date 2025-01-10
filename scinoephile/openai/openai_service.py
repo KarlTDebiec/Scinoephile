10f8e959c3fe3d7c7c49d133f12266c2dfc0aa26
Karl T Debiec <karl.t.debiec@gmail.com>
2025-01-10 18:39:48 -0500

    Update copyright (#89)

--- scinoephile/openai/openai_service.py
+++ scinoephile/openai/openai_service.py
@@ -1,4 +1,4 @@
-#  Copyright 2017-2024 Karl T Debiec. All rights reserved. This software may be modified
+#  Copyright 2017-2025 Karl T Debiec. All rights reserved. This software may be modified
 #  and distributed under the terms of the BSD license. See the LICENSE file for details.
 """Service for interacting with OpenAI API."""
 from __future__ import annotations
