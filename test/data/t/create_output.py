cad55bba773d0548ebbf25f5ee40162876d0289a
Karl Debiec <karl.t.debiec@gmail.com>
2025-01-10 21:11:23 -0500

    wip

--- test/data/t/create_output.py
+++ test/data/t/create_output.py
@@ -8,7 +8,11 @@ from pathlib import Path
 from scinoephile.common import package_root
 from scinoephile.core import Series
 from scinoephile.core.english import get_english_cleaned, get_english_flattened
-from scinoephile.core.hanzi import get_hanzi_flattened, get_hanzi_simplified
+from scinoephile.core.hanzi import (
+    get_hanzi_cleaned,
+    get_hanzi_flattened,
+    get_hanzi_simplified,
+)
 from scinoephile.core.synchronization import get_synced_series
 from scinoephile.testing.file import get_test_file_path
 
@@ -19,6 +23,8 @@ if __name__ == "__main__":
 
     # Simplified Standard Chinese
     cmn_hans_hk = Series.load(get_test_file_path("t/input/cmn-Hans-HK.srt"))
+    cmn_hans_hk_clean = get_hanzi_cleaned(cmn_hans_hk)
+    cmn_hans_hk_clean.save(get_output_path("t/output/cmn-Hans-HK_clean.srt"))
     cmn_hans_hk_flatten = get_hanzi_flattened(cmn_hans_hk)
     cmn_hans_hk_flatten.save(get_output_path("t/output/cmn-Hans-HK_flatten.srt"))
 
