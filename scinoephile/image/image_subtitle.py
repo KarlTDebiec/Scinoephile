f1f222f698d504548f57ce56694ff2260f381cdf
Karl Debiec <karl.t.debiec@gmail.com>
2025-01-10 19:12:13 -0500

    wip

--- scinoephile/image/image_subtitle.py
+++ scinoephile/image/image_subtitle.py
@@ -7,10 +7,11 @@ from dataclasses import fields
 from typing import Any
 
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageChops
 
 from scinoephile.core import Subtitle
 from scinoephile.image.base64 import get_base64_image
+from scinoephile.image.drawing import get_image_of_text
 
 
 class ImageSubtitle(Subtitle):
@@ -27,20 +28,11 @@ class ImageSubtitle(Subtitle):
         super().__init__(**super_kwargs)
 
         self.data = data
-
-    @property
-    def image(self) -> Image:
-        """Image of subtitle."""
-        if not hasattr(self, "_image") or self._image is None:
-            self._image = Image.fromarray(self.data)
-        return self._image
-
-    @image.setter
-    def image(self, image: Image) -> None:
-        """Set image of subtitle."""
-        self._image = image
-        self.data = np.array(image)
-        self._base64 = None
+        self._base64: str | None = None
+        self._image: Image.Image | None = None
+        self._image_diff: Image.Image | None = None
+        self._image_from_text: Image.Image | None = None
+        self._image_stack: Image.Image | None = None
 
     @property
     def base64(self) -> str:
@@ -48,3 +40,49 @@ class ImageSubtitle(Subtitle):
         if not hasattr(self, "_base64") or self._base64 is None:
             self._base64 = get_base64_image(self.image)
         return self._base64
+
+    @property
+    def image(self) -> Image.Image:
+        """Image of subtitle."""
+        if not hasattr(self, "_image") or self._image is None:
+            self._image = Image.fromarray(self.data)
+        return self._image
+
+    @image.setter
+    def image(self, image: Image.Image) -> None:
+        """Set image of subtitle."""
+        self._image = image
+        self.data = np.array(image)
+        self._base64 = None
+
+    @property
+    def image_from_text(self) -> Image.Image | None:
+        """Image generated from subtitle text, to compare with actual image."""
+        if self.text is None:
+            return None
+        if not hasattr(self, "_image_from_text") or self._image_from_text is None:
+            self._image_from_text = get_image_of_text(self.text, self.image.size)
+        return self._image_from_text
+
+    @property
+    def image_diff(self) -> Image.Image | None:
+        """Difference between actual and generated image, using pillow."""
+        if self.image_from_text is None:
+            return None
+        if not hasattr(self, "_image_diff") or self._image_diff is None:
+            self._image_diff = ImageChops.difference(self.image, self.image_from_text)
+        return self._image_diff
+
+    @property
+    def image_stack(self) -> Image.Image | None:
+        """Image, image generated from text and diff, vertically stacked."""
+        if self.image_from_text is None:
+            return None
+        if not hasattr(self, "_image_stack") or self._image_stack is None:
+            self._image_stack = Image.new(
+                "RGBA", (self.image.width, self.image.height * 3)
+            )
+            self._image_stack.paste(self.image, (0, 0))
+            self._image_stack.paste(self.image_from_text, (0, self.image.height))
+            self._image_stack.paste(self.image_diff, (0, self.image.height * 2))
+        return self._image_stack
