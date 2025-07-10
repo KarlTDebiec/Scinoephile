403b8970b34f85bdd991b44de3971d8d62760602
Karl Debiec <karl.t.debiec@gmail.com>
2025-07-10 17:37:23 -0400

    Simplify import statements in conftest.py by using wildcard imports

---
+++ test/conftest.py
@@ -0,0 +1,11 @@
+#  Copyright 2017-2025 Karl T Debiec. All rights reserved. This software may be modified
+#  and distributed under the terms of the BSD license. See the LICENSE file for details.
+"""Configuration of tests of scinoephile.core."""
+
+from __future__ import annotations
+
+# ruff: noqa: F401
+from test.data.kob import *
+from test.data.mnt import *
+from test.data.pdp import *
+from test.data.t import *
