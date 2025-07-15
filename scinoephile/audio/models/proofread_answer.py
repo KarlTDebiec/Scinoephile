ec66c90134956864b4152bd981d0276067216df9
Karl Debiec <karl.t.debiec@gmail.com>
2025-07-15 07:46:20 -0400

    Update copyright comments and refine field descriptions in 粤文 proofreading classes

--- scinoephile/audio/models/proofread_answer.py
+++ scinoephile/audio/models/proofread_answer.py
@@ -1,5 +1,5 @@
-# Copyright 2017-2025 Karl T Debiec. All rights reserved. This software may be modified
-# and distributed under the terms of the BSD license. See the LICENSE file for details.
+#  Copyright 2017-2025 Karl T Debiec. All rights reserved. This software may be modified
+#  and distributed under the terms of the BSD license. See the LICENSE file for details.
 """Answer for 粤文 proofreading."""
 
 from __future__ import annotations
@@ -12,5 +12,5 @@ from scinoephile.core.abcs import Answer
 class ProofreadAnswer(Answer):
     """Answer for 粤文 proofreading."""
 
-    yuewen_proofread: str = Field(..., description="Proofread 粤文 subtitle.")
+    yuewen_proofread: str = Field(..., description="Proofread 粤文 text.")
     note: str = Field(..., description="Description of corrections made.")
--- scinoephile/audio/models/proofread_query.py
+++ scinoephile/audio/models/proofread_query.py
@@ -1,5 +1,5 @@
-# Copyright 2017-2025 Karl T Debiec. All rights reserved. This software may be modified
-# and distributed under the terms of the BSD license. See the LICENSE file for details.
+#  Copyright 2017-2025 Karl T Debiec. All rights reserved. This software may be modified
+#  and distributed under the terms of the BSD license. See the LICENSE file for details.
 """Query for 粤文 proofreading."""
 
 from __future__ import annotations
@@ -12,5 +12,5 @@ from scinoephile.core.abcs import Query
 class ProofreadQuery(Query):
     """Query for 粤文 proofreading."""
 
-    zhongwen: str = Field(..., description="Known 中文 text of subtitle.")
-    yuewen: str = Field(..., description="Transcribed 粤文 subtitle to proofread.")
+    zhongwen: str = Field(..., description="Known 中文 text")
+    yuewen: str = Field(..., description="Transcribed 粤文 text to proofread.")
