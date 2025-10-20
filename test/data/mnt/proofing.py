4e369cbbcfbd7b52910291dba5b286655f5af6b9
Karl T Debiec <karl.t.debiec@gmail.com>
2025-10-20 15:12:34 +0800

    MNT 粤文 transcription part 1 (#302)

---
+++ test/data/mnt/proofing.py
@@ -0,0 +1,239 @@
+#  Copyright 2017-2025 Karl T Debiec. All rights reserved. This software may be modified
+#  and distributed under the terms of the BSD license. See the LICENSE file for details.
+"""Test data for MNT."""
+
+from __future__ import annotations
+
+from itertools import chain
+
+from scinoephile.audio.cantonese.proofing import ProofTestCase
+
+proof_test_cases_block_0 = [
+    ProofTestCase(
+        zhongwen="谢谢",
+        yuewen="食埋呢粒糖",
+        yuewen_proofread="",
+        note="The 粤文 '食埋呢粒糖' ('eat this candy') has no correspondence to "
+        "the 中文 '谢谢' ('thank you'), indicating a complete "
+        "mistranscription.",
+        difficulty=1,
+    ),
+    ProofTestCase(
+        zhongwen="妳们两个累不累啊？",
+        yuewen="之后啦，转埋呢个弯之后呢？",
+        yuewen_proofread="",
+        note="There is zero correspondence between the 粤文 and 中文 "
+        "subtitles, indicating a complete mistranscription.",
+        difficulty=1,
+    ),
+    ProofTestCase(
+        zhongwen="就快到了",
+        yuewen="就会到㗎啦",
+        yuewen_proofread="就会到㗎啦",
+        note="",
+    ),
+]  # proof_test_cases_block_0
+proof_test_cases_block_1 = [
+    ProofTestCase(
+        zhongwen="小梅，快躲起来",
+        yuewen="嗌，嗰个系边个呢",
+        yuewen_proofread="",
+        note="The 粤文 subtitle has no correspondence to the 中文 subtitle, "
+        "indicating a complete mistranscription.",
+        difficulty=1,
+    ),
+]  # proof_test_cases_block_1
+proof_test_cases_block_2 = []  # proof_test_cases_block_2
+proof_test_cases_block_3 = []  # proof_test_cases_block_3
+proof_test_cases_block_4 = []  # proof_test_cases_block_4
+proof_test_cases_block_5 = []  # proof_test_cases_block_5
+proof_test_cases_block_6 = []  # proof_test_cases_block_6
+proof_test_cases_block_7 = []  # proof_test_cases_block_7
+proof_test_cases_block_8 = []  # proof_test_cases_block_8
+proof_test_cases_block_9 = []  # proof_test_cases_block_9
+proof_test_cases_block_10 = []  # proof_test_cases_block_10
+proof_test_cases_block_11 = []  # proof_test_cases_block_11
+proof_test_cases_block_12 = []  # proof_test_cases_block_12
+proof_test_cases_block_13 = []  # proof_test_cases_block_13
+proof_test_cases_block_14 = []  # proof_test_cases_block_14
+proof_test_cases_block_15 = []  # proof_test_cases_block_15
+proof_test_cases_block_16 = []  # proof_test_cases_block_16
+proof_test_cases_block_17 = []  # proof_test_cases_block_17
+proof_test_cases_block_18 = []  # proof_test_cases_block_18
+proof_test_cases_block_19 = []  # proof_test_cases_block_19
+proof_test_cases_block_20 = []  # proof_test_cases_block_20
+proof_test_cases_block_21 = []  # proof_test_cases_block_21
+proof_test_cases_block_22 = []  # proof_test_cases_block_22
+proof_test_cases_block_23 = []  # proof_test_cases_block_23
+proof_test_cases_block_24 = []  # proof_test_cases_block_24
+proof_test_cases_block_25 = []  # proof_test_cases_block_25
+proof_test_cases_block_26 = []  # proof_test_cases_block_26
+proof_test_cases_block_27 = []  # proof_test_cases_block_27
+proof_test_cases_block_28 = []  # proof_test_cases_block_28
+proof_test_cases_block_29 = []  # proof_test_cases_block_29
+proof_test_cases_block_30 = []  # proof_test_cases_block_30
+proof_test_cases_block_31 = []  # proof_test_cases_block_31
+proof_test_cases_block_32 = []  # proof_test_cases_block_32
+proof_test_cases_block_33 = []  # proof_test_cases_block_33
+proof_test_cases_block_34 = []  # proof_test_cases_block_34
+proof_test_cases_block_35 = []  # proof_test_cases_block_35
+proof_test_cases_block_36 = []  # proof_test_cases_block_36
+proof_test_cases_block_37 = []  # proof_test_cases_block_37
+proof_test_cases_block_38 = []  # proof_test_cases_block_38
+proof_test_cases_block_39 = []  # proof_test_cases_block_39
+proof_test_cases_block_40 = []  # proof_test_cases_block_40
+proof_test_cases_block_41 = []  # proof_test_cases_block_41
+proof_test_cases_block_42 = []  # proof_test_cases_block_42
+proof_test_cases_block_43 = []  # proof_test_cases_block_43
+proof_test_cases_block_44 = []  # proof_test_cases_block_44
+proof_test_cases_block_45 = []  # proof_test_cases_block_45
+proof_test_cases_block_46 = []  # proof_test_cases_block_46
+proof_test_cases_block_47 = []  # proof_test_cases_block_47
+proof_test_cases_block_48 = []  # proof_test_cases_block_48
+proof_test_cases_block_49 = []  # proof_test_cases_block_49
+proof_test_cases_block_50 = []  # proof_test_cases_block_50
+proof_test_cases_block_51 = []  # proof_test_cases_block_51
+proof_test_cases_block_52 = []  # proof_test_cases_block_52
+proof_test_cases_block_53 = []  # proof_test_cases_block_53
+proof_test_cases_block_54 = []  # proof_test_cases_block_54
+proof_test_cases_block_55 = []  # proof_test_cases_block_55
+proof_test_cases_block_56 = []  # proof_test_cases_block_56
+proof_test_cases_block_57 = []  # proof_test_cases_block_57
+proof_test_cases_block_58 = []  # proof_test_cases_block_58
+proof_test_cases_block_59 = []  # proof_test_cases_block_59
+proof_test_cases_block_60 = []  # proof_test_cases_block_60
+proof_test_cases_block_61 = []  # proof_test_cases_block_61
+proof_test_cases_block_62 = []  # proof_test_cases_block_62
+proof_test_cases_block_63 = []  # proof_test_cases_block_63
+proof_test_cases_block_64 = []  # proof_test_cases_block_64
+proof_test_cases_block_65 = []  # proof_test_cases_block_65
+proof_test_cases_block_66 = []  # proof_test_cases_block_66
+proof_test_cases_block_67 = []  # proof_test_cases_block_67
+proof_test_cases_block_68 = []  # proof_test_cases_block_68
+proof_test_cases_block_69 = []  # proof_test_cases_block_69
+proof_test_cases_block_70 = []  # proof_test_cases_block_70
+proof_test_cases_block_71 = []  # proof_test_cases_block_71
+proof_test_cases_block_72 = []  # proof_test_cases_block_72
+proof_test_cases_block_73 = []  # proof_test_cases_block_73
+proof_test_cases_block_74 = []  # proof_test_cases_block_74
+proof_test_cases_block_75 = []  # proof_test_cases_block_75
+proof_test_cases_block_76 = []  # proof_test_cases_block_76
+proof_test_cases_block_77 = []  # proof_test_cases_block_77
+proof_test_cases_block_78 = []  # proof_test_cases_block_78
+proof_test_cases_block_79 = []  # proof_test_cases_block_79
+proof_test_cases_block_80 = []  # proof_test_cases_block_80
+proof_test_cases_block_81 = []  # proof_test_cases_block_81
+proof_test_cases_block_82 = []  # proof_test_cases_block_82
+proof_test_cases_block_83 = []  # proof_test_cases_block_83
+proof_test_cases_block_84 = []  # proof_test_cases_block_84
+proof_test_cases_block_85 = []  # proof_test_cases_block_85
+proof_test_cases_block_86 = []  # proof_test_cases_block_86
+proof_test_cases_block_87 = []  # proof_test_cases_block_87
+proof_test_cases_block_88 = []  # proof_test_cases_block_88
+proof_test_cases_block_89 = []  # proof_test_cases_block_89
+proof_test_cases_block_90 = []  # proof_test_cases_block_90
+proof_test_cases_block_91 = []  # proof_test_cases_block_91
+proof_test_cases_block_92 = []  # proof_test_cases_block_92
+proof_test_cases_block_93 = []  # proof_test_cases_block_93
+proof_test_cases_block_94 = []  # proof_test_cases_block_94
+proof_test_cases_block_95 = []  # proof_test_cases_block_95
+proof_test_cases_block_96 = []  # proof_test_cases_block_96
+proof_test_cases_block_97 = []  # proof_test_cases_block_97
+proof_test_cases_block_98 = []  # proof_test_cases_block_98
+proof_test_cases_block_99 = []  # proof_test_cases_block_99
+proof_test_cases_block_100 = []  # proof_test_cases_block_100
+proof_test_cases_block_101 = []  # proof_test_cases_block_101
+proof_test_cases_block_102 = []  # proof_test_cases_block_102
+proof_test_cases_block_103 = []  # proof_test_cases_block_103
+proof_test_cases_block_104 = []  # proof_test_cases_block_104
+proof_test_cases_block_105 = []  # proof_test_cases_block_105
+proof_test_cases_block_106 = []  # proof_test_cases_block_106
+proof_test_cases_block_107 = []  # proof_test_cases_block_107
+proof_test_cases_block_108 = []  # proof_test_cases_block_108
+proof_test_cases_block_109 = []  # proof_test_cases_block_109
+proof_test_cases_block_110 = []  # proof_test_cases_block_110
+proof_test_cases_block_111 = []  # proof_test_cases_block_111
+proof_test_cases_block_112 = []  # proof_test_cases_block_112
+proof_test_cases_block_113 = []  # proof_test_cases_block_113
+proof_test_cases_block_114 = []  # proof_test_cases_block_114
+proof_test_cases_block_115 = []  # proof_test_cases_block_115
+proof_test_cases_block_116 = []  # proof_test_cases_block_116
+proof_test_cases_block_117 = []  # proof_test_cases_block_117
+proof_test_cases_block_118 = []  # proof_test_cases_block_118
+proof_test_cases_block_119 = []  # proof_test_cases_block_119
+proof_test_cases_block_120 = []  # proof_test_cases_block_120
+proof_test_cases_block_121 = []  # proof_test_cases_block_121
+proof_test_cases_block_122 = []  # proof_test_cases_block_122
+proof_test_cases_block_123 = []  # proof_test_cases_block_123
+proof_test_cases_block_124 = []  # proof_test_cases_block_124
+proof_test_cases_block_125 = []  # proof_test_cases_block_125
+proof_test_cases_block_126 = []  # proof_test_cases_block_126
+proof_test_cases_block_127 = []  # proof_test_cases_block_127
+proof_test_cases_block_128 = []  # proof_test_cases_block_128
+proof_test_cases_block_129 = []  # proof_test_cases_block_129
+proof_test_cases_block_130 = []  # proof_test_cases_block_130
+proof_test_cases_block_131 = []  # proof_test_cases_block_131
+proof_test_cases_block_132 = []  # proof_test_cases_block_132
+proof_test_cases_block_133 = []  # proof_test_cases_block_133
+proof_test_cases_block_134 = []  # proof_test_cases_block_134
+proof_test_cases_block_135 = []  # proof_test_cases_block_135
+proof_test_cases_block_136 = []  # proof_test_cases_block_136
+proof_test_cases_block_137 = []  # proof_test_cases_block_137
+proof_test_cases_block_138 = []  # proof_test_cases_block_138
+proof_test_cases_block_139 = []  # proof_test_cases_block_139
+proof_test_cases_block_140 = []  # proof_test_cases_block_140
+proof_test_cases_block_141 = []  # proof_test_cases_block_141
+proof_test_cases_block_142 = []  # proof_test_cases_block_142
+proof_test_cases_block_143 = []  # proof_test_cases_block_143
+proof_test_cases_block_144 = []  # proof_test_cases_block_144
+proof_test_cases_block_145 = []  # proof_test_cases_block_145
+proof_test_cases_block_146 = []  # proof_test_cases_block_146
+proof_test_cases_block_147 = []  # proof_test_cases_block_147
+proof_test_cases_block_148 = []  # proof_test_cases_block_148
+proof_test_cases_block_149 = []  # proof_test_cases_block_149
+proof_test_cases_block_150 = []  # proof_test_cases_block_150
+proof_test_cases_block_151 = []  # proof_test_cases_block_151
+proof_test_cases_block_152 = []  # proof_test_cases_block_152
+proof_test_cases_block_153 = []  # proof_test_cases_block_153
+proof_test_cases_block_154 = []  # proof_test_cases_block_154
+proof_test_cases_block_155 = []  # proof_test_cases_block_155
+proof_test_cases_block_156 = []  # proof_test_cases_block_156
+proof_test_cases_block_157 = []  # proof_test_cases_block_157
+proof_test_cases_block_158 = []  # proof_test_cases_block_158
+proof_test_cases_block_159 = []  # proof_test_cases_block_159
+proof_test_cases_block_160 = []  # proof_test_cases_block_160
+proof_test_cases_block_161 = []  # proof_test_cases_block_161
+proof_test_cases_block_162 = []  # proof_test_cases_block_162
+proof_test_cases_block_163 = []  # proof_test_cases_block_163
+proof_test_cases_block_164 = []  # proof_test_cases_block_164
+proof_test_cases_block_165 = []  # proof_test_cases_block_165
+proof_test_cases_block_166 = []  # proof_test_cases_block_166
+proof_test_cases_block_167 = []  # proof_test_cases_block_167
+proof_test_cases_block_168 = []  # proof_test_cases_block_168
+proof_test_cases_block_169 = []  # proof_test_cases_block_169
+proof_test_cases_block_170 = []  # proof_test_cases_block_170
+proof_test_cases_block_171 = []  # proof_test_cases_block_171
+proof_test_cases_block_172 = []  # proof_test_cases_block_172
+proof_test_cases_block_173 = []  # proof_test_cases_block_173
+proof_test_cases_block_174 = []  # proof_test_cases_block_174
+proof_test_cases_block_175 = []  # proof_test_cases_block_175
+proof_test_cases_block_176 = []  # proof_test_cases_block_176
+proof_test_cases_block_177 = []  # proof_test_cases_block_177
+proof_test_cases_block_178 = []  # proof_test_cases_block_178
+proof_test_cases_block_179 = []  # proof_test_cases_block_179
+proof_test_cases_block_180 = []  # proof_test_cases_block_180
+proof_test_cases_block_181 = []  # proof_test_cases_block_181
+
+mnt_proof_test_cases: list[ProofTestCase] = list(
+    chain.from_iterable(
+        cases
+        for name, cases in globals().items()
+        if name.startswith("proof_test_cases_block_") and cases
+    )
+)
+
+"""MNT 粤文 proof test cases."""
+
+__all__ = [
+    "mnt_proof_test_cases",
+]
---
+++ test/data/mnt/shifting.py
@@ -0,0 +1,225 @@
+#  Copyright 2017-2025 Karl T Debiec. All rights reserved. This software may be modified
+#  and distributed under the terms of the BSD license. See the LICENSE file for details.
+"""Test data for MNT."""
+
+from __future__ import annotations
+
+from itertools import chain
+
+from scinoephile.audio.cantonese.shifting import ShiftTestCase
+
+shift_test_cases_block_0 = [
+    ShiftTestCase(
+        zhongwen_1="谢谢",
+        yuewen_1="",
+        zhongwen_2="妳们两个累不累啊？",
+        yuewen_2="食埋呢粒糖之后啦",
+        yuewen_1_shifted="食埋呢粒糖",
+        yuewen_2_shifted="之后啦",
+        difficulty=1,
+    ),
+    ShiftTestCase(
+        zhongwen_1="妳们两个累不累啊？",
+        yuewen_1="之后啦",
+        zhongwen_2="就快到了",
+        yuewen_2="转埋呢个弯之后呢就会到㗎啦",
+        yuewen_1_shifted="之后啦转埋呢个弯之后呢",
+        yuewen_2_shifted="就会到㗎啦",
+        difficulty=1,
+    ),
+]  # shift_test_cases_block_0
+shift_test_cases_block_1 = []  # shift_test_cases_block_1
+shift_test_cases_block_2 = []  # shift_test_cases_block_2
+shift_test_cases_block_3 = []  # shift_test_cases_block_3
+shift_test_cases_block_4 = []  # shift_test_cases_block_4
+shift_test_cases_block_5 = []  # shift_test_cases_block_5
+shift_test_cases_block_6 = []  # shift_test_cases_block_6
+shift_test_cases_block_7 = []  # shift_test_cases_block_7
+shift_test_cases_block_8 = []  # shift_test_cases_block_8
+shift_test_cases_block_9 = []  # shift_test_cases_block_9
+shift_test_cases_block_10 = []  # shift_test_cases_block_10
+shift_test_cases_block_11 = []  # shift_test_cases_block_11
+shift_test_cases_block_12 = []  # shift_test_cases_block_12
+shift_test_cases_block_13 = []  # shift_test_cases_block_13
+shift_test_cases_block_14 = []  # shift_test_cases_block_14
+shift_test_cases_block_15 = []  # shift_test_cases_block_15
+shift_test_cases_block_16 = []  # shift_test_cases_block_16
+shift_test_cases_block_17 = []  # shift_test_cases_block_17
+shift_test_cases_block_18 = []  # shift_test_cases_block_18
+shift_test_cases_block_19 = []  # shift_test_cases_block_19
+shift_test_cases_block_20 = []  # shift_test_cases_block_20
+shift_test_cases_block_21 = []  # shift_test_cases_block_21
+shift_test_cases_block_22 = []  # shift_test_cases_block_22
+shift_test_cases_block_23 = []  # shift_test_cases_block_23
+shift_test_cases_block_24 = []  # shift_test_cases_block_24
+shift_test_cases_block_25 = []  # shift_test_cases_block_25
+shift_test_cases_block_26 = []  # shift_test_cases_block_26
+shift_test_cases_block_27 = []  # shift_test_cases_block_27
+shift_test_cases_block_28 = []  # shift_test_cases_block_28
+shift_test_cases_block_29 = []  # shift_test_cases_block_29
+shift_test_cases_block_30 = []  # shift_test_cases_block_30
+shift_test_cases_block_31 = []  # shift_test_cases_block_31
+shift_test_cases_block_32 = []  # shift_test_cases_block_32
+shift_test_cases_block_33 = []  # shift_test_cases_block_33
+shift_test_cases_block_34 = []  # shift_test_cases_block_34
+shift_test_cases_block_35 = []  # shift_test_cases_block_35
+shift_test_cases_block_36 = []  # shift_test_cases_block_36
+shift_test_cases_block_37 = []  # shift_test_cases_block_37
+shift_test_cases_block_38 = []  # shift_test_cases_block_38
+shift_test_cases_block_39 = []  # shift_test_cases_block_39
+shift_test_cases_block_40 = []  # shift_test_cases_block_40
+shift_test_cases_block_41 = []  # shift_test_cases_block_41
+shift_test_cases_block_42 = []  # shift_test_cases_block_42
+shift_test_cases_block_43 = []  # shift_test_cases_block_43
+shift_test_cases_block_44 = []  # shift_test_cases_block_44
+shift_test_cases_block_45 = []  # shift_test_cases_block_45
+shift_test_cases_block_46 = []  # shift_test_cases_block_46
+shift_test_cases_block_47 = []  # shift_test_cases_block_47
+shift_test_cases_block_48 = []  # shift_test_cases_block_48
+shift_test_cases_block_49 = []  # shift_test_cases_block_49
+shift_test_cases_block_50 = []  # shift_test_cases_block_50
+shift_test_cases_block_51 = []  # shift_test_cases_block_51
+shift_test_cases_block_52 = []  # shift_test_cases_block_52
+shift_test_cases_block_53 = []  # shift_test_cases_block_53
+shift_test_cases_block_54 = []  # shift_test_cases_block_54
+shift_test_cases_block_55 = []  # shift_test_cases_block_55
+shift_test_cases_block_56 = []  # shift_test_cases_block_56
+shift_test_cases_block_57 = []  # shift_test_cases_block_57
+shift_test_cases_block_58 = []  # shift_test_cases_block_58
+shift_test_cases_block_59 = []  # shift_test_cases_block_59
+shift_test_cases_block_60 = []  # shift_test_cases_block_60
+shift_test_cases_block_61 = []  # shift_test_cases_block_61
+shift_test_cases_block_62 = []  # shift_test_cases_block_62
+shift_test_cases_block_63 = []  # shift_test_cases_block_63
+shift_test_cases_block_64 = []  # shift_test_cases_block_64
+shift_test_cases_block_65 = []  # shift_test_cases_block_65
+shift_test_cases_block_66 = []  # shift_test_cases_block_66
+shift_test_cases_block_67 = []  # shift_test_cases_block_67
+shift_test_cases_block_68 = []  # shift_test_cases_block_68
+shift_test_cases_block_69 = []  # shift_test_cases_block_69
+shift_test_cases_block_70 = []  # shift_test_cases_block_70
+shift_test_cases_block_71 = []  # shift_test_cases_block_71
+shift_test_cases_block_72 = []  # shift_test_cases_block_72
+shift_test_cases_block_73 = []  # shift_test_cases_block_73
+shift_test_cases_block_74 = []  # shift_test_cases_block_74
+shift_test_cases_block_75 = []  # shift_test_cases_block_75
+shift_test_cases_block_76 = []  # shift_test_cases_block_76
+shift_test_cases_block_77 = []  # shift_test_cases_block_77
+shift_test_cases_block_78 = []  # shift_test_cases_block_78
+shift_test_cases_block_79 = []  # shift_test_cases_block_79
+shift_test_cases_block_80 = []  # shift_test_cases_block_80
+shift_test_cases_block_81 = []  # shift_test_cases_block_81
+shift_test_cases_block_82 = []  # shift_test_cases_block_82
+shift_test_cases_block_83 = []  # shift_test_cases_block_83
+shift_test_cases_block_84 = []  # shift_test_cases_block_84
+shift_test_cases_block_85 = []  # shift_test_cases_block_85
+shift_test_cases_block_86 = []  # shift_test_cases_block_86
+shift_test_cases_block_87 = []  # shift_test_cases_block_87
+shift_test_cases_block_88 = []  # shift_test_cases_block_88
+shift_test_cases_block_89 = []  # shift_test_cases_block_89
+shift_test_cases_block_90 = []  # shift_test_cases_block_90
+shift_test_cases_block_91 = []  # shift_test_cases_block_91
+shift_test_cases_block_92 = []  # shift_test_cases_block_92
+shift_test_cases_block_93 = []  # shift_test_cases_block_93
+shift_test_cases_block_94 = []  # shift_test_cases_block_94
+shift_test_cases_block_95 = []  # shift_test_cases_block_95
+shift_test_cases_block_96 = []  # shift_test_cases_block_96
+shift_test_cases_block_97 = []  # shift_test_cases_block_97
+shift_test_cases_block_98 = []  # shift_test_cases_block_98
+shift_test_cases_block_99 = []  # shift_test_cases_block_99
+shift_test_cases_block_100 = []  # shift_test_cases_block_100
+shift_test_cases_block_101 = []  # shift_test_cases_block_101
+shift_test_cases_block_102 = []  # shift_test_cases_block_102
+shift_test_cases_block_103 = []  # shift_test_cases_block_103
+shift_test_cases_block_104 = []  # shift_test_cases_block_104
+shift_test_cases_block_105 = []  # shift_test_cases_block_105
+shift_test_cases_block_106 = []  # shift_test_cases_block_106
+shift_test_cases_block_107 = []  # shift_test_cases_block_107
+shift_test_cases_block_108 = []  # shift_test_cases_block_108
+shift_test_cases_block_109 = []  # shift_test_cases_block_109
+shift_test_cases_block_110 = []  # shift_test_cases_block_110
+shift_test_cases_block_111 = []  # shift_test_cases_block_111
+shift_test_cases_block_112 = []  # shift_test_cases_block_112
+shift_test_cases_block_113 = []  # shift_test_cases_block_113
+shift_test_cases_block_114 = []  # shift_test_cases_block_114
+shift_test_cases_block_115 = []  # shift_test_cases_block_115
+shift_test_cases_block_116 = []  # shift_test_cases_block_116
+shift_test_cases_block_117 = []  # shift_test_cases_block_117
+shift_test_cases_block_118 = []  # shift_test_cases_block_118
+shift_test_cases_block_119 = []  # shift_test_cases_block_119
+shift_test_cases_block_120 = []  # shift_test_cases_block_120
+shift_test_cases_block_121 = []  # shift_test_cases_block_121
+shift_test_cases_block_122 = []  # shift_test_cases_block_122
+shift_test_cases_block_123 = []  # shift_test_cases_block_123
+shift_test_cases_block_124 = []  # shift_test_cases_block_124
+shift_test_cases_block_125 = []  # shift_test_cases_block_125
+shift_test_cases_block_126 = []  # shift_test_cases_block_126
+shift_test_cases_block_127 = []  # shift_test_cases_block_127
+shift_test_cases_block_128 = []  # shift_test_cases_block_128
+shift_test_cases_block_129 = []  # shift_test_cases_block_129
+shift_test_cases_block_130 = []  # shift_test_cases_block_130
+shift_test_cases_block_131 = []  # shift_test_cases_block_131
+shift_test_cases_block_132 = []  # shift_test_cases_block_132
+shift_test_cases_block_133 = []  # shift_test_cases_block_133
+shift_test_cases_block_134 = []  # shift_test_cases_block_134
+shift_test_cases_block_135 = []  # shift_test_cases_block_135
+shift_test_cases_block_136 = []  # shift_test_cases_block_136
+shift_test_cases_block_137 = []  # shift_test_cases_block_137
+shift_test_cases_block_138 = []  # shift_test_cases_block_138
+shift_test_cases_block_139 = []  # shift_test_cases_block_139
+shift_test_cases_block_140 = []  # shift_test_cases_block_140
+shift_test_cases_block_141 = []  # shift_test_cases_block_141
+shift_test_cases_block_142 = []  # shift_test_cases_block_142
+shift_test_cases_block_143 = []  # shift_test_cases_block_143
+shift_test_cases_block_144 = []  # shift_test_cases_block_144
+shift_test_cases_block_145 = []  # shift_test_cases_block_145
+shift_test_cases_block_146 = []  # shift_test_cases_block_146
+shift_test_cases_block_147 = []  # shift_test_cases_block_147
+shift_test_cases_block_148 = []  # shift_test_cases_block_148
+shift_test_cases_block_149 = []  # shift_test_cases_block_149
+shift_test_cases_block_150 = []  # shift_test_cases_block_150
+shift_test_cases_block_151 = []  # shift_test_cases_block_151
+shift_test_cases_block_152 = []  # shift_test_cases_block_152
+shift_test_cases_block_153 = []  # shift_test_cases_block_153
+shift_test_cases_block_154 = []  # shift_test_cases_block_154
+shift_test_cases_block_155 = []  # shift_test_cases_block_155
+shift_test_cases_block_156 = []  # shift_test_cases_block_156
+shift_test_cases_block_157 = []  # shift_test_cases_block_157
+shift_test_cases_block_158 = []  # shift_test_cases_block_158
+shift_test_cases_block_159 = []  # shift_test_cases_block_159
+shift_test_cases_block_160 = []  # shift_test_cases_block_160
+shift_test_cases_block_161 = []  # shift_test_cases_block_161
+shift_test_cases_block_162 = []  # shift_test_cases_block_162
+shift_test_cases_block_163 = []  # shift_test_cases_block_163
+shift_test_cases_block_164 = []  # shift_test_cases_block_164
+shift_test_cases_block_165 = []  # shift_test_cases_block_165
+shift_test_cases_block_166 = []  # shift_test_cases_block_166
+shift_test_cases_block_167 = []  # shift_test_cases_block_167
+shift_test_cases_block_168 = []  # shift_test_cases_block_168
+shift_test_cases_block_169 = []  # shift_test_cases_block_169
+shift_test_cases_block_170 = []  # shift_test_cases_block_170
+shift_test_cases_block_171 = []  # shift_test_cases_block_171
+shift_test_cases_block_172 = []  # shift_test_cases_block_172
+shift_test_cases_block_173 = []  # shift_test_cases_block_173
+shift_test_cases_block_174 = []  # shift_test_cases_block_174
+shift_test_cases_block_175 = []  # shift_test_cases_block_175
+shift_test_cases_block_176 = []  # shift_test_cases_block_176
+shift_test_cases_block_177 = []  # shift_test_cases_block_177
+shift_test_cases_block_178 = []  # shift_test_cases_block_178
+shift_test_cases_block_179 = []  # shift_test_cases_block_179
+shift_test_cases_block_180 = []  # shift_test_cases_block_180
+shift_test_cases_block_181 = []  # shift_test_cases_block_181
+
+mnt_shift_test_cases: list[ShiftTestCase] = list(
+    chain.from_iterable(
+        cases
+        for name, cases in globals().items()
+        if name.startswith("shift_test_cases_block_") and cases
+    )
+)
+
+"""MNT 粤文 shifting test cases."""
+
+__all__ = [
+    "mnt_shift_test_cases",
+]
