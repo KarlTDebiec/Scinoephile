19429f7bd5db20da0b1dca717f193ed843d2432d
Karl Debiec <karl.t.debiec@gmail.com>
2025-12-18 16:16:10 -0500

    wip

--- scinoephile/llms/base/__init__.py
+++ scinoephile/llms/base/__init__.py
@@ -1,6 +1,6 @@
 #  Copyright 2017-2025 Karl T Debiec. All rights reserved. This software may be modified
 #  and distributed under the terms of the BSD license. See the LICENSE file for details.
-"""Core code for working with LLMs."""
+"""Base related to interactions with LLMs."""
 
 from __future__ import annotations
 
