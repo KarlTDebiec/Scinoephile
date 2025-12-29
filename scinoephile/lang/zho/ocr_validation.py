f4ab68d13d1f851b8426d6066594c40d32873f84
Karl Debiec <karl.t.debiec@gmail.com>
2025-12-29 10:22:01 -0500

    wip

--- scinoephile/lang/zho/ocr_validation.py
+++ scinoephile/lang/zho/ocr_validation.py
@@ -17,20 +17,26 @@ __all__ = ["validate_zho_ocr"]
 def validate_zho_ocr(
     series: ImageSeries,
     output_dir_path: Path | str | None = None,
+    stop_at_idx: int | None = None,
 ) -> None:
     """Validate OCR text against image series bboxes.
 
     Arguments:
         series: ImageSeries to validate
-        output_dir_path: Directory in which to save validation images
+        output_dir_path: directory in which to save validation images
+        stop_at_idx: stop processing at this index
     """
     bbox_mgr = BboxManager()
     output_series = ImageSeries() if output_dir_path is not None else None
-    for sub_idx, sub in enumerate(series, 1):
+    stop_at_idx = stop_at_idx or len(series)
+    for sub_idx, sub in enumerate(series):
+        if sub_idx >= stop_at_idx:
+            break
+        display_idx = sub_idx + 1
         if sub.bboxes is None:
             sub.bboxes = bbox_mgr.get_bboxes(sub, interactive=False)
         if sub.bboxes is None:
-            warning(f"Subtitle {sub_idx:04d}: {sub.text} - no bboxes found.")
+            warning(f"Subtitle {display_idx:04d}: {sub.text} - no bboxes found.")
             if output_series is not None:
                 output_series.events.append(
                     ImageSubtitle(
@@ -44,11 +50,11 @@ def validate_zho_ocr(
             continue
         if len(sub.text_without_whitspace) != len(sub.bboxes):
             warning(
-                f"Subtitle {sub_idx:04d}: {sub.text} - text has "
+                f"Subtitle {display_idx:04d}: {sub.text} - text has "
                 f"{len(sub.text_without_whitspace)} non-whitespace characters, "
                 f"but image has {len(sub.bboxes)} bboxes."
             )
-        for message in bbox_mgr.validate_char_bboxes(sub, subtitle_index=sub_idx):
+        for message in bbox_mgr.validate_char_bboxes(sub, subtitle_index=display_idx):
             warning(message)
         if output_dir_path is not None:
             annotated_img = get_img_with_bboxes(sub.img_with_white_bg, sub.bboxes)
