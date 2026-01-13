9382eb68b9e3e61de2a3e555cda6ab2dd2526387
Karl Debiec <karl.t.debiec@gmail.com>
2026-01-13 10:53:45 -0500

    wip

--- test/data/t/create_output.py
+++ test/data/t/create_output.py
@@ -36,9 +36,9 @@ output_dir = title_root / "output"
 set_logging_verbosity(2)
 
 actions = {
-    # "繁體中文 (OCR)",
+    "繁體中文 (OCR)",
     "简体中文 (OCR)",
-    # "English (OCR)",
+    "English (OCR)",
     # "简体中文 (SRT)",
     # "繁體中文 (SRT)",
     # "English (SRT)",
