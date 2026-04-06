7d274242375ced7631418b8808e7668bae9a26b2
Karl Debiec <karl.t.debiec@gmail.com>
2026-04-06 14:02:43 -0400

    wip

---
+++ scinoephile/core/dictionaries/__init__.py
@@ -0,0 +1,17 @@
+#  Copyright 2017-2026 Karl T Debiec. All rights reserved. This software may be modified
+#  and distributed under the terms of the BSD license. See the LICENSE file for details.
+"""Core dictionary models and shared lookup metadata."""
+
+from __future__ import annotations
+
+from .dictionary_definition import DictionaryDefinition
+from .dictionary_entry import DictionaryEntry
+from .dictionary_source import DictionarySource
+from .lookup_direction import LookupDirection
+
+__all__ = [
+    "DictionaryDefinition",
+    "DictionaryEntry",
+    "DictionarySource",
+    "LookupDirection",
+]
---
+++ scinoephile/core/dictionaries/dictionary_definition.py
@@ -0,0 +1,22 @@
+#  Copyright 2017-2026 Karl T Debiec. All rights reserved. This software may be modified
+#  and distributed under the terms of the BSD license. See the LICENSE file for details.
+"""Model class for dictionary definitions."""
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+__all__ = [
+    "DictionaryDefinition",
+]
+
+
+@dataclass(frozen=True)
+class DictionaryDefinition:
+    """Definition metadata tied to a dictionary entry."""
+
+    text: str
+    """Definition text associated with the dictionary entry."""
+
+    label: str = ""
+    """Optional part-of-speech or usage label for the definition."""
