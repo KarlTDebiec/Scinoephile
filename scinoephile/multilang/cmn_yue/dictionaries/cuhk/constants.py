d5341df1afc2151af34312a8276617b4acab6cba
Karl Debiec <karl.t.debiec@gmail.com>
2026-04-06 14:26:27 -0400

    wip

--- scinoephile/multilang/cmn_yue/dictionaries/cuhk/constants.py
+++ scinoephile/multilang/cmn_yue/dictionaries/cuhk/constants.py
@@ -1,6 +1,6 @@
 #  Copyright 2017-2026 Karl T Debiec. All rights reserved. This software may be modified
 #  and distributed under the terms of the BSD license. See the LICENSE file for details.
-"""Constants used by the CUHK dictionary scraper and service."""
+"""Constants used by the CUHK dictionary."""
 
 from __future__ import annotations
 
