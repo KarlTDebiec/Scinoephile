51a425f1a031076b1002c42f47561150394ed362
Karl Debiec <karl.t.debiec@gmail.com>
2026-05-05 11:49:21 -0400

    Stop exporting OCR validation manager from OCR package

--- scinoephile/image/ocr/__init__.py
+++ scinoephile/image/ocr/__init__.py
@@ -1,9 +1,3 @@
 #  Copyright 2017-2026 Karl T Debiec. All rights reserved. This software may be modified
 #  and distributed under the terms of the BSD license. See the LICENSE file for details.
 """OCR-related image processing."""
-
-from __future__ import annotations
-
-from .validation import ValidationManager
-
-__all__ = ["ValidationManager"]
