2fe64026e1dcd59f28da283f85aafd59bb66e311
Karl Debiec <karl.t.debiec@gmail.com>
2026-05-06 12:03:25 -0400

    Keep OCR validation move separate from PaddleOCR

--- scinoephile/image/ocr/validation/__init__.py
+++
@@ -1,9 +0,0 @@
-#  Copyright 2017-2026 Karl T Debiec. All rights reserved. This software may be modified
-#  and distributed under the terms of the BSD license. See the LICENSE file for details.
-"""OCR validation for image subtitles."""
-
-from __future__ import annotations
-
-from .validation_manager import ValidationManager
-
-__all__ = ["ValidationManager"]
