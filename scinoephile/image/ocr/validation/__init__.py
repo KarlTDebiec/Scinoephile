202a008f6075a0735aef21573a11e381141bf4e1
Karl Debiec <karl.t.debiec@gmail.com>
2026-05-06 12:00:47 -0400

    Move OCR validation into validation package

--- scinoephile/image/ocr/validation/__init__.py
+++ scinoephile/image/ocr/validation/__init__.py
@@ -1,3 +1,9 @@
 #  Copyright 2017-2026 Karl T Debiec. All rights reserved. This software may be modified
 #  and distributed under the terms of the BSD license. See the LICENSE file for details.
-"""OCR-related image processing."""
+"""OCR validation for image subtitles."""
+
+from __future__ import annotations
+
+from .validation_manager import ValidationManager
+
+__all__ = ["ValidationManager"]
