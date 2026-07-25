7ef715b3c2d968f8a9f5955148039a176f8d52c6
Karl Debiec <karl.t.debiec@gmail.com>
2026-07-25 15:25:50 -0400

    Simplify MLX-Audio imports

--- scinoephile/audio/transcription/__init__.py
+++ scinoephile/audio/transcription/__init__.py
@@ -23,10 +23,7 @@ from .exceptions import (
     TranscriptionError,
     TranscriptionInferenceError,
 )
-from .mlx_audio import (
-    MlxAudioModelProfile,
-    MlxAudioTranscriber,
-)
+from .mlx_audio import MlxAudioModelProfile, MlxAudioTranscriber
 from .transcribed_segment import TranscribedSegment
 from .transcribed_word import TranscribedWord
 from .whisper_transcriber import WhisperTranscriber
