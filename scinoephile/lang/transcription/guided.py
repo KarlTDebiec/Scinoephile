37e8b59e434e62606083d325f26226883bb35606
Karl T Debiec <karl.t.debiec@gmail.com>
2026-08-05 11:54:26 -0400

    Default guided transcription Demucs to off (#1224)

--- scinoephile/lang/transcription/guided.py
+++ scinoephile/lang/transcription/guided.py
@@ -186,7 +186,7 @@ def get_guided_transcriber(
     *,
     model_name: str | None = None,
     backend: TranscriptionBackend = TranscriptionBackend.WHISPER,
-    demucs_mode: DemucsMode = DemucsMode.AUTO,
+    demucs_mode: DemucsMode = DemucsMode.OFF,
     vad_mode: VADMode = VADMode.OFF,
     cache_root_path: Path | None = None,
     overwrite_cache: bool = False,
